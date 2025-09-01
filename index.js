ed1f75627aefb630277ad03e2d8d3570ab61cfa2
Kushagra Tiwari <kushagratiwari86@gmail.com>
2025-09-01 22:38:29 +0530

    Fetching Pokemon images

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-fetch
+/*
+fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+.then(response=>
+    {
+        if(!response.ok)
+            throw new Error("Pokemon name or something else is wrong");
+        return response.json();
+    }
+)
+.then(data=>console.log(data.id))
+.catch(error=>console.error(error));
+*/
+
+
+
+//Using Async and await
+
+//fetchData();
+
+
+
+
+async function fetchData() {
+    try{
+
+        const pokemonName=document.getElementById("pokemon-name").value.toLowerCase();
+
+        const response =await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+            if(!response.ok)
+                throw new Error("Could not fetch")
+            const data=await response.json()
+            //console.log(data);
+            const pokemonImage=data.sprites.front_default;
+            const imageElement=document.getElementById("pokemon-image");
+            imageElement.src=pokemonImage;
+            imageElement.style.display="block";
+    }
+    catch(error){
+        console.error(error)
+    }
+}
