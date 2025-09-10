9fdd6962db6e1ad10e68817bb0301157a9a2d6e2
Kushagra Tiwari <kushagratiwari86@gmail.com>
2025-09-11 02:39:41 +0530

    Completed Till 17.55

--- notes/index.js
+++ notes/index.js
@@ -9,10 +9,26 @@ app.use(express.static(path.join(__dirname,'public')))
 
 
 
+
+
 app.get("/",function(req,res)
 {
     res.render("index");
 }) 
+
+//Dynamic routes
+
+app.get("/profile/:username",function(req,res)
+{
+    res.send(`Welcome babby, ${req.params.username}`);
+})
+
+app.get("/profile/:username/:age",function(req,res)
+{
+    res.send(`Yoo Babbyy of age ${req.params.age} and name ${req.params.username}`)
+})
+
+
 app.listen(3000,function(){
     console.log("It's running");
 })
